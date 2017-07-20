6f1431b7638798db00fcb44f6d2691ea06718467
Кирилл <98kika12@mail.ru>
2017-07-20 12:02:03 +0300

    Add: Profile page, api profile, model angular (UserInfo), add profile.component.

---
+++ InstructionIO/InstructionIO/wwwroot/app/model/UserInfo.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var UserInfo = (function () {
+    function UserInfo(Id, FirstName, LastName, Birthday, Avatar, Interests) {
+        this.Id = Id;
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.Birthday = Birthday;
+        this.Avatar = Avatar;
+        this.Interests = Interests;
+    }
+    return UserInfo;
+}());
+exports.UserInfo = UserInfo;
+//export class UserInfo {
+//    constructor(public firstName: string, public lastName: string) {
+//    }
+//} 
+//# sourceMappingURL=UserInfo.js.map
